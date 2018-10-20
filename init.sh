465edeb5b894f4477432d2cb2b9283642f78d614
Mathias Edouin <mathias.edouin@efrei.net>
2018-10-20 19:14:14 +0200

    Amélioration du petit script d'init

--- init.sh
+++ init.sh
@@ -3,12 +3,24 @@
 
 echo "=====Script d'initialisation====="
 
+workdir="$(pwd)"
 
-buildir="$(pwd)/build"
-echo "Dossier à effacer :" $buildir
+echo "Dossier actuel :" $workdir
 
-rm -rf $buildir
-truffle compile
-truffle migrate
+read -p "Le script requiert l'ouverture de Ganache, l'avez-vous bien ouvert [O/n] ? : " answer
+
+if [ -d "$workdir/build" ]; then
+  echo "Le dossier build existe, suppression..."
+  rm -rf $workdir/build
+  echo "Suppression effectuée"
+fi
+
+if [ "$answer" = "O" -o "$answer" = "o" ]
+then
+  truffle compile
+  truffle migrate
+else
+  echo "Vous devez ouvrir Ganache avant de déclencher le script"
+fi
 
 exit
