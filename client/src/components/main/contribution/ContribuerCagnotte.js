ecd7094374f46051d2a58b5ea85220b55c5903ce
Alexis Gigleux <mathias.edouin@efrei.net>
2018-10-25 13:42:35 +0200

    Ajout de la popover de contribution à une cagnotte
    Ajout d'un input montant
    Ajout d'une text area
    Ajout d'un bouton

--- client/src/components/main/contribution/ContribuerCagnotte.js
+++ client/src/components/main/contribution/ContribuerCagnotte.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
-import {  Button } from 'reactstrap';
+import { Col, Button, Popover, PopoverHeader, PopoverBody, InputGroup, Input, InputGroupAddon, Label, FormGroup, Form } from 'reactstrap';
+
+
 
 /**
  * ContribuerCagnotte
  */
 export class ContribuerCagnotte extends Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      popoverOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      popoverOpen: !this.state.popoverOpen
+    });
+  }
+
   render() {
     return (
-      <Button color="primary">Contribuer Cagnotte</Button>
+      <div>
+        <Button id="PopoverCagnotte" color="primary" onClick={this.toggle}>
+          Contribuer Cagnotte
+        </Button>
+        <Popover placement="auto" isOpen={this.state.popoverOpen} target="PopoverCagnotte" toggle={this.toggle}>
+          <PopoverHeader>Votre contribution</PopoverHeader>
+          <PopoverBody>
+          <Col lg={12}>
+            <InputGroup>
+              <Input placeholder="Montant" />
+              <InputGroupAddon addonType="append">mc2icoins</InputGroupAddon>
+            </InputGroup>
+            <Form>
+              <FormGroup>
+                <Input type="textarea" name="text" id="exampleText" placeholder="Votre message"/>
+              </FormGroup>
+            </Form>
+            <Button color="primary" onClick={this.toggle}>Valider</Button>
+          </Col>
+          </PopoverBody>
+        </Popover>
+      </div>
     );
   }
 }
